c1aff33de11806233151c5620593b154e50440d8
Jyotirmoy Konwar <jyotirmoykonwarjk@gmail.com>
2026-03-24 12:19:12 +0530

    get post

--- Flask/flask/getpost.py
+++ Flask/flask/getpost.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -6,13 +6,20 @@ app = Flask(__name__)
 def welcome():
     return "<html><h1>Welcome to da dawg house!</h1></html>"
 
-@app.route("/index")
+@app.route("/index",methods = ['GET'])
 def index():
     return render_template("index.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/form", methods = ['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Yo! {name}!'
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
